4b81a1f6bcd4a3925126d6fe6a9c2ec3e0f46cb5
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2012-12-30 00:33:51 +0900

    less to css

--- grunt.js
+++ grunt.js
@@ -28,9 +28,9 @@ grunt.loadNpmTasks('grunt-less');
 grunt.registerTask('default', 'watch');
 
 // less
-grunt.registerTask('less', 'less');
+//grunt.registerTask('less', 'less');
 
 // csso
-grunt.registerTask('csso', 'csso');
+//grunt.registerTask('csso', 'csso');
 
 };
